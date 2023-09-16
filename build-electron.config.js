1a9ecbb28ba80419985678168957c08608444264
Ugo Parlange <uparlange@gmail.com>
2023-09-16 19:20:56 +0200

    Release 1.2.2

--- build-electron.config.js
+++ build-electron.config.js
@@ -2,6 +2,6 @@ module.exports = {
   mainEntry: "src/main/electron-main.js",
   preloadEntry: "src/main/electron-preload.js",
   outDir: "build",
-  mainTarget: "electron26.0-main",
-  preloadTarget: "electron26.0-preload",
+  mainTarget: "electron26.2-main",
+  preloadTarget: "electron26.2-preload",
 }
